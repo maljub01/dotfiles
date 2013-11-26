maljub01/dotfiles

commit e009a84f428ac17964c93f779a01a48efb732d34

Fix issues with using vi as git's editor on Mac OS X.
Details similar to: http://andrewho.co.uk/weblog/vim-pathogen-with-mutt-and-git

vimrc
@@ -2,11 +2,11 @@
 " This must be first, because it changes other options as a side effect.
 set nocompatible
 
-"""""""""""
-" Vundle: "
-"""""""""""
-filetype off " Required.
+if has("filetype")
+  filetype off " Required for vundle. The if-statement prevents exiting with a non-zero status on Mac OS X.
+endif
 
+" Setup Vundle
 set rtp+=~/.vim/bundle/vundle/
 call vundle#rc()
 

commit 3c7eace10ff55cf1b8adaa5a7cb68b474e1956a8

Fixed a bug that broke Vundles' filetype detection
This bug was introduced by e009a84f428ac17964c93f779a01a48efb732d34.

vimrc
@@ -2,9 +2,8 @@
 " This must be first, because it changes other options as a side effect.
 set nocompatible
 
-if has("filetype")
-  filetype off " Required for vundle. The if-statement prevents exiting with a non-zero status on Mac OS X.
-endif
+filetype on
+filetype off " Required for vundle. 'filetype on' prevents exiting with a non-zero status on Mac OS X.
 
 " Setup Vundle
 set rtp+=~/.vim/bundle/vundle/